ff2a3e109a81005c4c3472a5fe540ab04b253cdf
weichonghit <weichonghit@gmail.com>
2020-11-18 20:10:27 +0800

    修改model, 添加卷积

--- model_define.py
+++ model_define.py
@@ -4,61 +4,83 @@ from torch import nn
 import torch.nn.functional as F
 from collections import OrderedDict
 
+class DownConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.down_conv = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride = 1), nn.BatchNorm2d(out_channels), nn.ReLU(inplace=True))
+        self.maxpooling = nn.MaxPool2d(kernel_size=2)
+    def forward(self, x):
+        x1 = self.down_conv(x)
+        x = self.maxpooling(x1)
+        return x, x1
 
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
-    def __init__(self, in_channels, out_channels, mid_channels=None):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(mid_channels), nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels), nn.ReLU(inplace=True))
+        self.double_conv = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), nn.BatchNorm2d(out_channels), nn.ReLU(inplace=True),
+                                         nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1), nn.BatchNorm2d(out_channels), nn.ReLU(inplace=True))
 
     def forward(self, x):
         return self.double_conv(x)
 
-
-class Down(nn.Module):
+class Encoder4(nn.Module):
     """Downscaling with maxpool then double conv"""
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2), DoubleConv(in_channels, out_channels))
+        self.conv0 = DoubleConv(in_channels,  out_channels)
+        self.conv1 = DoubleConv(out_channels, out_channels)
+        self.conv2 = DoubleConv(out_channels, out_channels)
+        self.maxpooling = nn.MaxPool2d(kernel_size=2)
 
     def forward(self, x):
-        return self.maxpool_conv(x)
+        # 卷积块，6层
+        x  = self.conv0(x)
+        x1 = self.conv1(x)
+        x1 = x1 + x
+        x1 = self.conv2(x1)
+        # 池化
+        x = self.maxpooling(x1)
+        return x, x1
 
+class Encoder5(nn.Module):
+    """Downscaling with maxpool then double conv"""
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv0 = DoubleConv(in_channels,  out_channels)
+        self.conv1 = DoubleConv(out_channels, out_channels)
+        self.conv2 = DoubleConv(out_channels, out_channels)
+        self.conv3 = DoubleConv(out_channels, out_channels)
+        self.conv4 = DoubleConv(out_channels, out_channels)
+        self.conv5 = DoubleConv(out_channels, out_channels)
 
-class Up(nn.Module):
+    def forward(self, x):
+        # 卷积块，12层
+        x  = self.conv0(x)
+        x1 = self.conv1(x)
+        x1 = x1 + x
+        x2 = self.conv2(x1)
+        x2 = x2 + x1
+        x3  = self.conv3(x2)
+        x3 = x3 + x2
+        x4 = self.conv4(x3)
+        x4 = x4 + x3
+        x4 = self.conv5(x4)
+        return x4
+
+class UpConv(nn.Module):
     """Upscaling then double conv"""
-    def __init__(self, in_channels, out_channels, bilinear=True):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
 
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2,
-                                  mode='bilinear',
-                                  align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels,
-                                         in_channels // 2,
-                                         kernel_size=2,
-                                         stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride = 1), nn.BatchNorm2d(out_channels), nn.ReLU(inplace=True))
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(
-            x1,
-            [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
         x = torch.cat([x2, x1], dim=1)
+
         return self.conv(x)
 
 
@@ -72,34 +94,34 @@ class OutConv(nn.Module):
 
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=True):
+    def __init__(self, n_channels = 3, n_classes = 18):
         super(UNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
-        self.bilinear = bilinear
+        self.encoder1 = DownConv(3, 32)  # 256^2 -> 128^2
+        self.encoder2 = DownConv(32, 64) # 128^2 -> 64^2
+        self.encoder3 = DownConv(64, 128) # 64^2 -> 32^2
+        self.encoder4 = Encoder4(128,256) # 32^2 -> 16^2
+        self.encoder5 = Encoder5(256,512) # 16^2 -> 16^2
+        
+        self.decoder4 = UpConv(512 + 256, 256) 
+        self.decoder3 = UpConv(256 + 128, 128)
+        self.decoder2 = UpConv(128 + 64, 64)
+        self.decoder1 = UpConv(64 + 32, 32)
 
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
-        factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+        self.outc = OutConv(32, n_classes)
 
     def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
+        x, x1 = self.encoder1(x)
+        x, x2 = self.encoder2(x)
+        x, x3 = self.encoder3(x)
+        x, x4 = self.encoder4(x)
+        x = self.encoder5(x)
+        x  = self.decoder4(x, x4)
+        x  = self.decoder3(x, x3)
+        x  = self.decoder2(x, x2)
+        x  = self.decoder1(x, x1)
+        
         logits = self.outc(x)
         return logits
 
