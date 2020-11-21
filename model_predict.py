7b6829d4b56f6b3701ad3aff541baec928136a30
Zweo <exzhouwei@outlook.com>
2020-11-21 15:51:57 +0800

    1

--- model_predict.py
+++ model_predict.py
@@ -1,8 +1,9 @@
 import os
-import cv2
+# import cv2
 import torch
 import numpy as np
 from PIL import Image
+# from torch.nn import modules
 
 
 def cutImage(file_name):
@@ -31,22 +32,37 @@ def mergeImage(n, imgs, o_size, n_size):
 def predict(model, input_path, output_dir):
     name, _ = os.path.splitext(input_path)
     name = os.path.split(name)[-1] + ".png"
-    img = cv2.imread(input_path).astype(np.float32)
+    n, imgs, o_size, n_size = cutImage(input_path)
+    res = []
+    for img in imgs:
+        img = torch.from_numpy(np.asarray(img)).float().unsqueeze(0)
+        if torch.cuda.is_available():
+            img = img.cuda()
+        img = img.permute(0, 3, 1, 2)
+        label = model(img)
+        label = torch.argmax(label,
+                             dim=1).cpu().squeeze().numpy().astype(np.uint8)
+        res.append(Image.fromarray(label))
+    img = mergeImage(n, res, o_size, n_size)
+    img.save(os.path.join(output_dir, name))
 
-    # ox, oy = img.shape[0], img.shape[1]
-    # img = cv2.resize(img, (256, 256))
-    # img = torch.from_numpy(img).unsqueeze(0)
-    # if torch.cuda.is_available():
-    #     img = img.cuda()
-    # img = img.permute(0, 3, 1, 2)
-    # label = model(img)
-    # label = torch.argmax(label, dim=1).cpu().squeeze().numpy().astype(np.uint8)
-    # label = cv2.resize(label, (ox, oy))
 
-    cv2.imwrite(os.path.join(output_dir, name), label)
-
+# def predict(model, input_path, output_dir):
+#     name, _ = os.path.splitext(input_path)
+#     name = os.path.split(name)[-1] + ".png"
+#     img = cv2.imread(input_path).astype(np.float32)
+#     ox, oy = img.shape[0], img.shape[1]
+#     img = cv2.resize(img, (256, 256))
+#     img = torch.from_numpy(img).unsqueeze(0)
+#     if torch.cuda.is_available():
+#         img = img.cuda()
+#     img = img.permute(0, 3, 1, 2)
+#     label = model(img)
+#     label = torch.argmax(label, dim=1).cpu().squeeze().numpy().astype(np.uint8)
+#     label = cv2.resize(label, (ox, oy))
+#     cv2.imwrite(os.path.join(output_dir, name), label)
 
 if __name__ == "__main__":
     from model_define import init_model
-    mode = init_model()
-    predict(mode, '../data/images/1.tif', '')
+    model = init_model()
+    predict(model, '../data/images/1.tif', '')
